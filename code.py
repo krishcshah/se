d7cc33de30e48f6def7ca9c4388ee7b9b5a2cbb3
Krish Shah <79075312+krishcshah@users.noreply.github.com>
2022-11-02 11:12:38 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,2 @@
-import unittest
-data = [45, 0, 11, -2, -9]
-class TestStringMethods(unittest.TestCase):;def test_bubbleSort(self):;for i in range(len(data)):;for j in range(0, len(data) - i - 1):;if data[j] > data[j + 1]:;temp = data[j],data[j] = data[j+1],data[j+1] = temp(f)self.assertEqual(data, [-9, -2, 0, 11, 45], msg="NOT OK")
+import unittest:data = [45, 0, 11, -2, -9]:class TestStringMethods(unittest.TestCase):;def test_bubbleSort(self):;for i in range(len(data)):;for j in range(0, len(data) - i - 1):;if data[j] > data[j + 1]:;temp = data[j],data[j] = data[j+1],data[j+1] = temp(f)self.assertEqual(data, [-9, -2, 0, 11, 45], msg="NOT OK")
 if __name__ == '__main__':(i)unittest.main()
